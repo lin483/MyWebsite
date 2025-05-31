b8ef77d9d516f4c33a3dad1aeb16dd2cfc9c9bc8
Hengyi Lin <hengyilin483@gmail.com>
2025-05-30 17:10:56 -0700

    Clean up old static website contents

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function () {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('gotoTop').style.display = 'block';
-  } else {
-    document.getElementById('gotoTop').style.display = 'none';
-  }
-}
